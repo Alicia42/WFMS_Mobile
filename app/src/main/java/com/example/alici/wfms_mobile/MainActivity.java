7dc5bf465a659dc725b4f74f4d5128ca45c8c332
Alicia Craig <aliciacraig42@gmail.com>
2017-09-11 14:37:32 +1200

    Started on login page and adding a new customer page.

--- app/src/main/java/com/example/alici/wfms_mobile/MainActivity.java
+++ app/src/main/java/com/example/alici/wfms_mobile/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.alici.wfms_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(this, newCustomerFormActivity.class);
+        startActivity(intent);
     }
 }
