a03bda80a73b872a9e418f0ac1d8e98f76a7f933
libbyjenn97 <libby_jennings@hotmail.co.nz>
2017-09-20 11:10:51 +1200

    Added booVariable class

---
+++ app/src/main/java/com/example/alici/wfms_mobile/BooVariable.java
@@ -0,0 +1,31 @@
+package com.example.alici.wfms_mobile;
+
+/**
+ * Created by libbyjennings on 18/09/17.
+ */
+
+public class BooVariable {
+    private boolean boo = false;
+    private ChangeListener listener;
+
+    public boolean isBoo() {
+        return boo;
+    }
+
+    public void setBoo(boolean boo) {
+        this.boo = boo;
+        if (listener != null) listener.onChange();
+    }
+
+    public ChangeListener getListener() {
+        return listener;
+    }
+
+    public void setListener(ChangeListener listener) {
+        this.listener = listener;
+    }
+
+    public interface ChangeListener {
+        void onChange();
+    }
+}
